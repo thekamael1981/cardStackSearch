1d3e778940a07e36c175d1af618fcc2db09f458e
Kamael1981 <rengkahkamael@gmail.com>
2025-08-15 11:02:38 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: path.resolve(__dirname, 'client'), // Lokasi index.html
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'client', 'src'),
+      '@shared': path.resolve(__dirname, 'shared'),
+      '@assets': path.resolve(__dirname, 'attached_assets'),
+    },
+  },
   build: {
-    outDir: 'dist',      // folder output build default
-    emptyOutDir: true,   // hapus isi folder sebelum build
+    outDir: path.resolve(__dirname, 'dist'), // hasil build di root/dist
+    emptyOutDir: true,
+  },
+  server: {
+    fs: {
+      strict: true,
+      deny: ['**/.*'],
+    },
   },
 })
